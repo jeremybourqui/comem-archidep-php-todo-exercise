a7644ebd6596f1fb14fee0195e7ac34c6495caa7
Alan <alan.rodriguez@heig-vd.ch>
2020-10-06 11:27:03 +0200

    Add toggle query

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = "UPDATE todo SET done = IF(done=1, 0, 1) WHERE id=$id;"; // IMPLEMENT ME
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
